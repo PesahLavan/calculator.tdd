c71aa7d00161536a555b8ae0a3b12f6ef03d689c
Svyatoslav Chatchenko <mydogtom@gmail.com>
2016-07-26 22:36:48 +0200

    tried to create a test. something wrong with project structure and folders. test cannot see the class under the test

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,8 @@
 public class Main {
-
+    /**
+     * Accepts file name, reads instructions from file name, reports output in form of "Result = XXX"
+     * @param args
+     */
     public static void main(String[] args) {
 
     }
