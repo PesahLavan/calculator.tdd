8f65ee5bf2c0a7b88da4ceaad563fc35def298cb
Pavel <pesahlavan@mail.ru>
2016-08-04 12:33:40 +0200

    add InstructionConnect class

--- src/test/java/com/example/calculator/CalculatorTest.java
+++ src/test/java/com/example/calculator/CalculatorTest.java
@@ -7,22 +7,25 @@ import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 
 public class CalculatorTest {
+    private final String path = "/test/resources/Example1.txt";
+    InstructionConnect mockConnector;
     Calculator mCalculator;
     CalculatorReporter mockReporter;
     CalculatorFileReader mockReader;
     @Before
     public void setUp() throws Exception {
+        mockConnector = mock(InstructionConnect.class);
         mockReporter = mock(CalculatorReporter.class);
         mockReader = mock(CalculatorFileReader.class);
         mCalculator = new Calculator(mockReporter, mockReader);
-
     }
+
+
 
     @Test
     public void shouldPrintSummary() throws Exception {
-        String path = "/test/resources/Example1.txt";
+
         mCalculator.calculate(path);
         verify(mockReporter).print(42);
-
     }
 }
