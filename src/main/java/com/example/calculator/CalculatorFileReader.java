c54c6b11c2d1ad8ffe9c6638445827c641311c5d
Pavel <pesahlavan@mail.ru>
2016-08-04 15:04:57 +0200

    realise InstructionConnect.class

--- src/main/java/com/example/calculator/CalculatorFileReader.java
+++ src/main/java/com/example/calculator/CalculatorFileReader.java
@@ -1,8 +1,16 @@
 package com.example.calculator;
 
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class CalculatorFileReader {
 
-    public String readeLine(InstructionConnect connect) {
-        return new String("");
+    private List<Object> listInstruction = new ArrayList<>();
+
+    public List<Object> readeLine(InstructionConnect connect) {
+        listInstruction.add(new NotImplementedException());
+        return  listInstruction;
     }
 }
--- src/main/java/com/example/calculator/InstructionConnect.java
+++ src/main/java/com/example/calculator/InstructionConnect.java
@@ -1,7 +1,14 @@
 package com.example.calculator;
 
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Scanner;
+
 public class InstructionConnect {
-    public InstructionConnect connect(String path) {
-        return new InstructionConnect();
+    public Scanner connect(String name) throws IOException {
+        Path path = Paths.get(name);
+        Scanner readSca = new Scanner(path);
+        return readSca;
     }
 }
